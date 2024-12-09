fc24927e3b43212f308ab0f790830c952f8167af
josantiagoc <159163152+josantiagoc@users.noreply.github.com>
2024-12-08 20:41:04 -0500

    Update Main1.py

--- Main1.py
+++ Main1.py
@@ -6,14 +6,14 @@ from Direccion import Direccion
 agenda = Agenda(5)
 
 # usuario 1
-user1 = Usuario("Juan-Pérez", "24567898")
+user1 = Usuario("Juan-Perez", "24567898")
 fecha_user1 = Fecha("12", "10", "1980")
 user1.setFecha_nacimiento(fecha_user1)
-user1.setCiudad_nacimiento("Medellín")
+user1.setCiudad_nacimiento("Medellin")
 user1.setTel("3003233234")
 user1.setEmail("juanperez@edl.edu.co")
 direccion_user1 = Direccion()
-direccion_user1.setAll("kr74", "4T-35", "Boston", "Medellín", "null", "null")
+direccion_user1.setAll("kr74", "4T-35", "Boston", "Medellin", "null", "null")
 user1.setDir(direccion_user1)
 print(user1)
 
@@ -28,7 +28,7 @@ user2.setCiudad_nacimiento("Envigado")
 user2.setTel("3013234567")
 user2.setEmail("diegopalacio@edl.edu.co")
 direccion_user2 = Direccion()
-direccion_user2.setAll("cll65", "3-29", "Robledo", "Medellín", "Balcones-de-la-Quinta", "405")
+direccion_user2.setAll("cll65", "3-29", "Robledo", "Medellin", "Balcones-de-la-Quinta", "405")
 user2.setDir(direccion_user2)
 print(user2)
 
@@ -69,7 +69,7 @@ agenda.agregar(user4)
 user5 = Usuario("Tatiana-Ramirez", "2345934" )
 fecha_user5 = Fecha("15", "11", "1982")
 user5.setFecha_nacimiento(fecha_user5)
-user5.setCiudad_nacimiento("Medellín")
+user5.setCiudad_nacimiento("Medellin")
 user5.setTel("3004567890")
 user5.setEmail("tatianaramirez@edl.edu.co")
 direccion_user5 = Direccion()
@@ -81,3 +81,5 @@ print(user5)
 agenda.agregar(user5)
 
 agenda.buscar("1075689")#id de Pedro-Gomez retorna en numero donde esta ubicado en el arreglo
+
+agenda.toFile()
