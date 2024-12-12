67b82a3bb7a03b1e7e1e4112ea764cbe94411927
1StAndres <160525045+1StAndres@users.noreply.github.com>
2024-12-11 22:45:04 -0500

    Update Main2.py

--- Main2.py
+++ Main2.py
@@ -1,1 +1,16 @@
+from Agenda import Agenda
+from Usuario import Usuario
+# parte a
+agenda1 = Agenda(5)
+agenda1.importF()
 
+# parte b
+for i in range(5):
+    print(agenda1._registro[i].__str__())
+
+# parte c
+id = input("ingrese el id: ")
+agenda1.eliminar(id)
+
+# Parte d
+agenda1.toFile()
