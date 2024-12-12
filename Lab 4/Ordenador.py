7ddbaf5d1dcd187cb76c762af09f311c0c34a6fb
damazog <damazog@unal.edu.co>
2024-12-11 21:43:27 -0500

    Update Ordenador.py
    validación incluida

--- Lab 4/Ordenador.py
+++ Lab 4/Ordenador.py
@@ -23,10 +23,107 @@ class Ordenador:
         self._A[mayor] = temp2
         self._A[menor] = temp1
 
-    def imprimir(self):
-        print(self._A)
+    def ordenar_seleccion(self):
+        A = self._A
+        capacity = self._limit
+        
+        for i in range(capacity-1):
+            minIndex = i
+            for j in range(i+1,capacity):
+                if A[j] < A[minIndex]:
+                    minIndex = j
+            self.intercambiar(i, minIndex)
 
-numeritos = Ordenador(6)
-numeritos.imprimir()
-numeritos.ordenar_burbuja()
-numeritos.imprimir()
+    def ordenar_insercion(self):
+        A = self._A
+        capacity = self._limit
+
+        for i in range(1, capacity):
+            temp = A[i]
+            j = i
+            while j>0 and A[j-1]>temp:
+                A[j] = A[j-1]
+                j -= 1
+            A[j] = temp
+
+    ####
+    def ordenar_mergeSort(self):
+        self._mergeSort(0, self._limit - 1)
+
+    def _mergeSort(self, p, r):
+        if p < r:
+           q = (p + r) // 2   
+           self._mergeSort(p, q) 
+           self._mergeSort(q + 1, r)
+           self._merge(p, q, r)
+
+    def _merge(self, p, q, r):
+        left = self._A[p:q+1] 
+        right = self._A[q+1:r+1]  
+        i = j = 0
+        k = p
+
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]: 
+                self._A[k] = left[i]   
+                i += 1
+            else:
+                self._A[k] = right[j]
+                j += 1
+            k += 1
+        
+        while i < len(left):
+            self._A[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            self._A[k] = right[j]
+            j += 1
+            k += 1
+    
+    def mostrar(self):
+        print(self._A)
+    
+    def busqueda_binaria(self, x):
+        return self._busquedaBinaria(0, self._limit-1, x)
+    
+    def _busquedaBinaria(self, iLeft, iRight, x):
+        if iLeft > iRight:
+            return -1
+        mid = (iLeft + iRight)//2
+        if x < self._A[mid]:
+            return self._busquedaBinaria(iLeft, mid - 1, x)
+        elif x > self._A[mid]:
+            return self._busquedaBinaria(mid + 1, iRight, x)
+        else:
+            return mid
+   
+
+new = Ordenador(10)
+##burbuja
+new.inicializar()
+new.mostrar()
+new.ordenar_burbuja()
+new.mostrar()
+##selección
+new.inicializar()
+new.mostrar()
+new.ordenar_seleccion()
+new.mostrar()
+##inserción
+new.inicializar()
+new.mostrar()
+new.ordenar_insercion()
+new.mostrar()
+##MergeSort
+new.inicializar()
+new.mostrar()
+new.ordenar_mergeSort()
+new.mostrar()
+
+busqueda = new.busqueda_binaria(int(input("Introduzca el número que desea buscar: ")))
+if busqueda == -1:
+    print("El elemento no se encontró en el arreglo")
+else:
+    print("El elemento se encuentra en la posición: " + busqueda)
