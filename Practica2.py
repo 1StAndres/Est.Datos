d234a751101aa90b8149e14d78b5971168302870
1StAndres <160525045+1StAndres@users.noreply.github.com>
2025-03-04 17:51:38 -0500

    Update Practica2.py

--- Practica2.py
+++ Practica2.py
@@ -1,9 +1,18 @@
 import networkx as nt
 import pandas as pd
 
+def CaminoMasCortoTiempo(ciuA, ciuB, g):
+    return  nt.dijkstra_path(g, ciuA, ciuB, weight = "tiempo")
+
+
 url = "https://raw.githubusercontent.com/1StAndres/Est.Datos/refs/heads/main/Datos%20vias.csv"
 dataset = pd.read_csv(url, delimiter=';', skiprows=1, names=['CiudadA', 'CiudadB', 'KM', 'Minutos'])
 grafo = nt.Graph()
 
 for _, row in dataset.iterrows():
     grafo.add_edge(row['CiudadA'], row['CiudadB'], distancia=row['KM'], tiempo=row['Minutos'])
+
+a = input("Ingrese el nombre de la primera ciudad")
+b = input("Ingrese el nombre de la segunda ciudad")
+rutaT = CaminoMasCortoTiempo("Medellin","Manizales" ,grafo)
+print(f"La ruta mas corta segun el tiempo desde la ciudad {a} y la ciudad {b} es: {rutaT}")
