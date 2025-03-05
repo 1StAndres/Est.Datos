5fc614a49962558328e290ab60073d53938b3fb7
damazog <damazog@unal.edu.co>
2025-03-04 20:51:39 -0500

    están conectadas función
    duro

--- Practica2.py
+++ Practica2.py
@@ -12,7 +12,20 @@ grafo = nt.Graph()
 for _, row in dataset.iterrows():
     grafo.add_edge(row['CiudadA'], row['CiudadB'], distancia=row['KM'], tiempo=row['Minutos'])
 
-a = input("Ingrese el nombre de la primera ciudad")
-b = input("Ingrese el nombre de la segunda ciudad")
-rutaT = CaminoMasCortoTiempo("Medellin","Manizales" ,grafo)
+a = input("Ingrese el nombre de la primera ciudad: ")
+b = input("Ingrese el nombre de la segunda ciudad: ")
+rutaT = CaminoMasCortoTiempo("Medellin","Manizales" ,grafo) #¿no debería ser entre a y b?
 print(f"La ruta mas corta segun el tiempo desde la ciudad {a} y la ciudad {b} es: {rutaT}")
+
+
+#Función ¿están conectadas por una sola carretera?
+def estan_conectadas(ciuA, ciuB, g):
+    return g.has_edge(ciuA, ciuB)
+print("¿Están conectadas por una sola carretera?")
+x = input("Ingrese la primera ciudad: ")
+y = input("Ingrese la segunda ciudad: ")
+
+if estan_conectadas(x, y, grafo):
+    print(f"{x} y {y} están conectadas directamente por una carretera.")
+else:
+    print(f"{x} y {y} NO están conectadas directamente.")
